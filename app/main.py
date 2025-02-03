20821dc1c8fdf4656a7bada5bfb25fd03f5c671a
DineshKumar22MCA <dinesh1822.cbcs@gmail.com>
2025-02-03 13:29:26 +0530

    added a getting news dict_list_dict format

--- app/main.py
+++ app/main.py
@@ -1,16 +1,23 @@
 
-# app import files
+# fastapi
 from fastapi import FastAPI , Depends , HTTPException
-from sqlalchemy.orm import Session
-from app import models,schemas,crud,database 
-from typing import Optional
-from sqlalchemy import or_
 from fastapi import Query
-
+from typing import Optional
+# sqlalchemy
+from sqlalchemy.orm import Session
+from sqlalchemy import func
+from sqlalchemy import or_
+# fastapi_crud.app
+from app import models,schemas,crud,database 
+from sqlalchemy import or_
 # news_app import files
 from news_app.models import News 
 from pydantic import BaseModel
 from datetime import datetime
+from collections import defaultdict
+
+
+
 
 models.Base.metadata.create_all(bind=database.engine)
 
@@ -87,12 +94,76 @@ class NewsBase(BaseModel):
 
 
 
+
+# working endpoint get all news list[{dict}] format
 @app.get("/news", response_model= list[NewsBase] )
 def get_news(db : Session = Depends(get_db)):
     news_items = db.query(News).all()
     for item in news_items:
         print(item.news_id)
     return news_items
+
+
+
+# getting news dict of list of dict - py script
+@app.get("/getting_news_list_dict", response_model=dict[str, list[NewsBase]])
+def get_news_listdict(db: Session = Depends(get_db)):
+    all_news = db.query(News).all()
+
+    if not all_news:
+        raise HTTPException(status_code=404, detail="No news found")
+    
+    listdict_news = defaultdict(list)
+    for i in all_news:
+        listdict_news[i.query_name].append(i)
+
+    return dict(listdict_news)
+
+
+
+# not working endpoint
+# getting news like grouped news using sqlalchemy group function
+# @app.get("/getting_news_listdict_sqlalchemy", response_model=dict[str, list[NewsBase]])
+# def get_news_listdict_sqlalchemy(db: Session = Depends(get_db)):
+#     grouped_news = db.query(
+#         News.query_name, func.array_agg(News).label("news_list")
+#     ).group_by(News.query_name).all()
+
+#     listdict_news = {}
+
+#     for query_name, news_list in grouped_news:
+#         listdict_news[query_name] = news_list
+
+#     return listdict_news
+
+
+
+
+
+# working end point getting_news_list_dict
+# @app.get("/getting_news_list_dict", response_model=dict[str, list[NewsBase]])
+# def get_news_listdict(db: Session = Depends(get_db), query_name_filter: Optional[str] = None):
+#     query = db.query(News)
+    
+#     if query_name_filter:
+#         query = query.filter(News.query_name==query_name_filter)
+    
+#     all_news = query.all()
+    
+#     if not all_news:
+#         raise HTTPException(status_code=404, detail="No news found")
+    
+#     listdict_news = defaultdict(list)
+#     for i in all_news:
+#         listdict_news[i.query_name].append(i)
+
+#     return dict(listdict_news)
+
+
+
+
+
+
 
 
 
@@ -103,22 +174,6 @@ def get_news_by_id(news_id : int,db : Session = Depends(get_db)):
     if not news1:
         raise HTTPException(status_code=404 , detail="news not available")
     return news1
-
-
-
-
-# working endpoint1
-# @app.get("/search_news", response_model=list[NewsBase])
-# def search_news(title: Optional[str] = None , description: Optional[str] = None, db: Session = Depends(get_db),):
-#     query = db.query(News)
-    
-#     if title:
-#         query = query.filter(News.title.ilike(f"%{title}%"))
-#     if description:
-#         query = query.filter(News.description.ilike(f"%{description}%"))
-    
-#     news_items = query.all()
-#     return news_items
 
 
 
@@ -143,14 +198,25 @@ def search_news(search: Optional[str] = Query(None), db: Session = Depends(get_d
 
 
 
-# not working endpoint
-# @app.get("/search_news",response_class=list[NewsBase])
-# def search_news(title : Optional[str]=None, description:Optional[str]=None, db:Session=Depends(get_db)):
+# working endpoint1
+# @app.get("/search_news", response_model=list[NewsBase])
+# def search_news(title: Optional[str] = None , description: Optional[str] = None, db: Session = Depends(get_db),):
 #     query = db.query(News)
-
-#     if title or description:
-#         query = query.filter(News.title.ilike(f"%{title or description}%"))
     
+#     if title:
+#         query = query.filter(News.title.ilike(f"%{title}%"))
+#     if description:
+#         query = query.filter(News.description.ilike(f"%{description}%"))
     
 #     news_items = query.all()
 #     return news_items
+
+
+
+
+
+
+
+
+
+
